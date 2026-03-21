5694008b55e2bc46a3c22d325a191701f266f16c
Gaurav Pushpam <145956263+Gaurav85431@users.noreply.github.com>
2026-03-22 00:25:32 +0530

    component

--- App.tsx
+++ App.tsx
@@ -29,14 +29,48 @@ const App = () => {
       <Image source={img} style={{ width: 150, height: 150 }} /> */}
 
       {/* VARIABLE and DYNAMIC Values  lec 6*/}
-      <Text style={{ fontSize: 40 }}>Name: {name}</Text>
+      {/* <Text style={{ fontSize: 40 }}>Name: {name}</Text>
       <Text style={{ fontSize: 40 }}>Email: {email}</Text>
       <Text style={{ fontSize: 40 }}>Age: {age}</Text>
       <Text style={{ fontSize: 40 }}>Allowed Fruit: {someFruitName()}</Text>
       <Text style={{ fontSize: 40 }}>Add (20,30): {20 + 30}</Text>
       <Text style={{ fontSize: 40 }}>
         isElegible: {age > 18 ? 'Yes' : 'No'}
-      </Text>
+      </Text> */}
+
+      {/* Component in React Native Lec 7 */}
+      <Component />
+      {/* <UserComponent mname="Gaurav" mage={25} memail="gaurav@gmail.com" /> */}
+
+      <NewUserComponent />
+    </View>
+  );
+};
+
+const Component = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 20 }}>Name:Gaurav</Text>
+      <Text style={{ fontSize: 20 }}>Age:{age}</Text>
+      <Text style={{ fontSize: 20 }}>Email:{email}</Text>
+    </View>
+  );
+};
+
+// const UserComponent = ({ mname, mage, memail }) => {
+//   return (
+//     <View>
+//       <Text style={{ fontSize: 30 }}>My Name: {mname}</Text>
+//       <Text style={{ fontSize: 30 }}>My Age: {mage}</Text>
+//       <Text style={{ fontSize: 30 }}>My Email: {memail}</Text>
+//     </View>
+//   );
+// };
+
+const NewUserComponent = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>My Component: </Text>
     </View>
   );
 };
